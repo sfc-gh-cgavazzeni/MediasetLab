13e4268147ba01b395a556e4d907bc93401fc55a
Cristian Gavazzeni <cristian.gavazzeni@snowflake.com>
2026-03-31 17:07:26 +0200

    Update Gitintegration.sql
    new script

--- Gitintegration.sql
+++ Gitintegration.sql
@@ -1,8 +1,11 @@
+USE ROLE SYSADMIN;
+CREATE OR REPLACE DATABASE MEDIASET_LAB;
+
 CREATE OR REPLACE API INTEGRATION my_git_api_integration
   API_PROVIDER = git_https_api
   API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-cgavazzeni')
   ENABLED = TRUE;
 
-CREATE OR REPLACE GIT REPOSITORY my_db.my_schema.mediasetlab_repo
+CREATE OR REPLACE GIT REPOSITORY mediaset_lab.public.mediasetlab_repo
   API_INTEGRATION = my_git_api_integration
   ORIGIN = 'https://github.com/sfc-gh-cgavazzeni/MediasetLab.git';
